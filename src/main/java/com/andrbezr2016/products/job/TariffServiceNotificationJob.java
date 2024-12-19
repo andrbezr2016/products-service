5b6dc2aad0580b3747a1e46e7db4177ede28f9c2
Bezrukov Andrey <andrbezr2016@yandex.ru>
2024-12-19 16:11:24 +0300

    add sync with tariffs-service

--- src/main/java/com/andrbezr2016/products/job/TariffServiceNotificationJob.java
+++ src/main/java/com/andrbezr2016/products/job/TariffServiceNotificationJob.java
@@ -1,20 +1,37 @@
 package com.andrbezr2016.products.job;
 
 import com.andrbezr2016.products.client.TariffsServiceClient;
+import com.andrbezr2016.products.entity.TariffNotificationEntity;
+import com.andrbezr2016.products.mapper.TariffNotificationMapper;
+import com.andrbezr2016.products.repository.TariffNotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class TariffServiceNotificationJob implements Job {
 
+    private final TariffNotificationRepository tariffNotificationRepository;
     private final TariffsServiceClient tariffsServiceClient;
+    private final TariffNotificationMapper tariffNotificationMapper;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-
+        log.info("Start TariffServiceNotificationJob");
+        List<TariffNotificationEntity> tariffNotificationEntityList = tariffNotificationRepository.findAllByProcessedDateIsNull();
+        for (TariffNotificationEntity tariffNotificationEntity : tariffNotificationEntityList) {
+            log.info("Send notification with id: {}", tariffNotificationEntity.getId());
+            tariffsServiceClient.syncTariff(tariffNotificationMapper.toDto(tariffNotificationEntity));
+            tariffNotificationEntity.setProcessedDate(OffsetDateTime.now());
+            tariffNotificationRepository.save(tariffNotificationEntity);
+        }
     }
 }
