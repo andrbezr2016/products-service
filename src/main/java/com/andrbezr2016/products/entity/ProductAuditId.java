37d50a03101b9eacb13c7baf437b2ce4ce72d950
Bezrukov Andrey <andrbezr2016@yandex.ru>
2024-12-22 12:30:49 +0300

    Revert "add endDate"
    This reverts commit a70b2288ff9a0e1d9b6bf0f453e641c3d7f8b4f7.

--- src/main/java/com/andrbezr2016/products/entity/ProductAuditId.java
+++ src/main/java/com/andrbezr2016/products/entity/ProductAuditId.java
@@ -1,13 +1,9 @@
 package com.andrbezr2016.products.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class ProductAuditId {
 
