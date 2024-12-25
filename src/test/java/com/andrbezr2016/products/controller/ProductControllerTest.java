f2519408b60c7b834ced265bf8021bf2b4bcef99
Bezrukov Andrey <andrbezr2016@yandex.ru>
2024-12-25 13:57:28 +0300

    add tests 2

--- src/test/java/com/andrbezr2016/products/controller/ProductControllerTest.java
+++ src/test/java/com/andrbezr2016/products/controller/ProductControllerTest.java
@@ -1,18 +1,23 @@
 package com.andrbezr2016.products.controller;
 
 import com.andrbezr2016.products.dto.Product;
+import com.andrbezr2016.products.dto.ProductRequest;
+import com.andrbezr2016.products.entity.ProductEntity;
+import com.andrbezr2016.products.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -22,11 +27,17 @@ class ProductControllerTest {
     private final static String GET_CURRENT_VERSION = "/product/{id}/getCurrentVersion";
     private final static String GET_PREVIOUS_VERSIONS = "/product/{id}/getPreviousVersions";
     private final static String GET_VERSION_FOR_DATE = "/product/{id}/getVersionForDate";
+    private final static String CREATE_PRODUCT = "/product/create";
+    private final static String DELETE_PRODUCT = "/product/{id}/delete";
+    private final static String ROLL_BACK_VERSION = "/product/{id}/rollBackVersion";
+    private final static String SYNC_TARIFF = "/product/syncTariff";
 
     @Autowired
     MockMvc mvc;
     @Autowired
     ObjectMapper objectMapper;
+    @Autowired
+    ProductRepository productRepository;
 
     @Test
     void getCurrentVersionTest() throws Exception {
@@ -93,11 +104,44 @@ class ProductControllerTest {
     }
 
     @Test
-    void createProductTest() {
+    void createProductTest() throws Exception {
+        String name = "Product 2 Create";
+        UUID author = UUID.fromString("53d15658-5493-4828-80d9-f1c1f8eae252");
+        String description = "Product 2 description";
+        long version = 0L;
+
+        ProductRequest productRequest = ProductRequest.builder()
+                .name(name)
+                .type(Product.ProductType.LOAN)
+                .description(description)
+                .author(author)
+                .build();
+
+        mvc.perform(post(CREATE_PRODUCT).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(productRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.type").value(Product.ProductType.LOAN.toString()))
+                .andExpect(jsonPath("$.startDate").isNotEmpty())
+                .andExpect(jsonPath("$.endDate").isEmpty())
+                .andExpect(jsonPath("$.description").value(description))
+                .andExpect(jsonPath("$.author").value(author.toString()))
+                .andExpect(jsonPath("$.version").value(version));
     }
 
     @Test
-    void deleteProductTest() {
+    void deleteProductTest() throws Exception {
+        UUID id = UUID.fromString("15cfb4c1-7083-475e-838d-4a1e696cf917");
+
+        mvc.perform(delete(DELETE_PRODUCT, id))
+                .andExpect(status().isOk());
+
+        ProductEntity deletedProductEntity = productRepository.findCurrentVersionById(id).orElse(null);
+        assertNull(deletedProductEntity);
+
+        deletedProductEntity = productRepository.findLastVersionById(id).orElse(null);
+        assertNotNull(deletedProductEntity);
+        assertEquals(id, deletedProductEntity.getId());
+        assertEquals(ProductEntity.State.DELETED, deletedProductEntity.getState());
     }
 
     @Test
--- src/test/resources/db/migration/V10001__TestData.sql
+++ src/test/resources/db/migration/V10001__TestData.sql
@@ -2,3 +2,7 @@ INSERT INTO products (id, name, type, start_date, end_date, tariff, tariff_versi
 ('548ea2e0-bcef-4e12-b933-803a4de50106', 'Product 1 Create', 'CARD', '2020-01-01T12:00:00.000', '2020-01-01T13:00:00.000', null, null, '53d15658-5493-4828-80d9-f1c1f8eae252', 0, 'INACTIVE'),
 ('548ea2e0-bcef-4e12-b933-803a4de50106', 'Product 1 Update 1', 'CARD', '2020-01-01T13:00:00.000', '2020-01-01T14:00:00.000', '284add3b-e6f2-45f6-8a5e-1dfbed6a1f40', 2, '53d15658-5493-4828-80d9-f1c1f8eae252', 1, 'INACTIVE'),
 ('548ea2e0-bcef-4e12-b933-803a4de50106', 'Product 1 Update 2', 'CARD', '2020-01-01T14:00:00.000', null, '5c50cc6c-8600-48a3-acf8-a83298035857', 3, '53d15658-5493-4828-80d9-f1c1f8eae252', 2, 'ACTIVE');
+
+INSERT INTO products (id, name, type, start_date, end_date, tariff, tariff_version, author, version, state) VALUES
+('15cfb4c1-7083-475e-838d-4a1e696cf917', 'Product Del Create', 'LOAN', '2020-01-01T12:00:00.000', '2020-01-01T13:00:00.000', null, null, '53d15658-5493-4828-80d9-f1c1f8eae252', 0, 'INACTIVE'),
+('15cfb4c1-7083-475e-838d-4a1e696cf917', 'Product Del Update 1', 'LOAN', '2020-01-01T13:00:00.000', null, '38abfc79-bf9d-400a-808e-79256c28c401', 1, '53d15658-5493-4828-80d9-f1c1f8eae252', 1, 'ACTIVE');
