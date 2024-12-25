a719239ed139876c2320bde56c7cb1b0fc48d8e4
Bezrukov Andrey <andrbezr2016@yandex.ru>
2024-12-25 14:17:21 +0300

    add tests 3

--- src/test/java/com/andrbezr2016/products/controller/ProductControllerTest.java
+++ src/test/java/com/andrbezr2016/products/controller/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package com.andrbezr2016.products.controller;
 
 import com.andrbezr2016.products.dto.Product;
+import com.andrbezr2016.products.dto.ProductNotification;
 import com.andrbezr2016.products.dto.ProductRequest;
 import com.andrbezr2016.products.entity.ProductEntity;
 import com.andrbezr2016.products.repository.ProductRepository;
@@ -13,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -41,26 +43,18 @@ class ProductControllerTest {
 
     @Test
     void getCurrentVersionTest() throws Exception {
-        UUID id = UUID.fromString("548ea2e0-bcef-4e12-b933-803a4de50106");
-        UUID tariff = UUID.fromString("5c50cc6c-8600-48a3-acf8-a83298035857");
-        UUID author = UUID.fromString("53d15658-5493-4828-80d9-f1c1f8eae252");
-        String name = "Product 1 Update 2";
-        LocalDateTime startDate = LocalDateTime.parse("2020-01-01T14:00:00.000");
-        long version = 2L;
-        long tariffVersion = 3L;
-
         Product product = Product.builder()
-                .id(id)
-                .name(name)
+                .id(UUID.fromString("548ea2e0-bcef-4e12-b933-803a4de50106"))
+                .name("Product 1 Update 2")
                 .type(Product.ProductType.CARD)
-                .startDate(startDate)
-                .tariff(tariff)
-                .tariffVersion(tariffVersion)
-                .author(author)
-                .version(version)
+                .startDate(LocalDateTime.parse("2020-01-01T14:00:00.000"))
+                .tariff(UUID.fromString("5c50cc6c-8600-48a3-acf8-a83298035857"))
+                .tariffVersion(3L)
+                .author(UUID.fromString("53d15658-5493-4828-80d9-f1c1f8eae252"))
+                .version(2L)
                 .build();
 
-        mvc.perform(get(GET_CURRENT_VERSION, id))
+        mvc.perform(get(GET_CURRENT_VERSION, product.getId()))
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(product)));
     }
@@ -76,55 +70,44 @@ class ProductControllerTest {
 
     @Test
     void getVersionForDateTest() throws Exception {
-        UUID id = UUID.fromString("548ea2e0-bcef-4e12-b933-803a4de50106");
-        UUID tariff = UUID.fromString("284add3b-e6f2-45f6-8a5e-1dfbed6a1f40");
-        UUID author = UUID.fromString("53d15658-5493-4828-80d9-f1c1f8eae252");
-        String name = "Product 1 Update 1";
-        LocalDateTime startDate = LocalDateTime.parse("2020-01-01T13:00:00.000");
-        LocalDateTime endDate = LocalDateTime.parse("2020-01-01T14:00:00.000");
-        long version = 1L;
-        long tariffVersion = 2L;
         LocalDateTime date = LocalDateTime.parse("2020-01-01T13:30:00.000");
 
         Product product = Product.builder()
-                .id(id)
-                .name(name)
+                .id(UUID.fromString("548ea2e0-bcef-4e12-b933-803a4de50106"))
+                .name("Product 1 Update 1")
                 .type(Product.ProductType.CARD)
-                .startDate(startDate)
-                .endDate(endDate)
-                .tariff(tariff)
-                .tariffVersion(tariffVersion)
-                .author(author)
-                .version(version)
+                .startDate(LocalDateTime.parse("2020-01-01T13:00:00.000"))
+                .endDate(LocalDateTime.parse("2020-01-01T14:00:00.000"))
+                .tariff(UUID.fromString("284add3b-e6f2-45f6-8a5e-1dfbed6a1f40"))
+                .tariffVersion(2L)
+                .author(UUID.fromString("53d15658-5493-4828-80d9-f1c1f8eae252"))
+                .version(1L)
                 .build();
 
-        mvc.perform(get(GET_VERSION_FOR_DATE, id).queryParam("date", date.toString()))
+        mvc.perform(get(GET_VERSION_FOR_DATE, product.getId()).queryParam("date", date.toString()))
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(product)));
     }
 
     @Test
     void createProductTest() throws Exception {
-        String name = "Product 2 Create";
-        UUID author = UUID.fromString("53d15658-5493-4828-80d9-f1c1f8eae252");
-        String description = "Product 2 description";
         long version = 0L;
 
         ProductRequest productRequest = ProductRequest.builder()
-                .name(name)
+                .name("Product 2 Create")
                 .type(Product.ProductType.LOAN)
-                .description(description)
-                .author(author)
+                .description("Product 2 description")
+                .author(UUID.fromString("53d15658-5493-4828-80d9-f1c1f8eae252"))
                 .build();
 
         mvc.perform(post(CREATE_PRODUCT).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(productRequest)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value(name))
-                .andExpect(jsonPath("$.type").value(Product.ProductType.LOAN.toString()))
+                .andExpect(jsonPath("$.name").value(productRequest.getName()))
+                .andExpect(jsonPath("$.type").value(productRequest.getType().toString()))
                 .andExpect(jsonPath("$.startDate").isNotEmpty())
                 .andExpect(jsonPath("$.endDate").isEmpty())
-                .andExpect(jsonPath("$.description").value(description))
-                .andExpect(jsonPath("$.author").value(author.toString()))
+                .andExpect(jsonPath("$.description").value(productRequest.getDescription()))
+                .andExpect(jsonPath("$.author").value(productRequest.getAuthor().toString()))
                 .andExpect(jsonPath("$.version").value(version));
     }
 
@@ -145,10 +128,50 @@ class ProductControllerTest {
     }
 
     @Test
-    void rollBackVersionTest() {
+    void rollBackVersionTest() throws Exception {
+        UUID id = UUID.fromString("272fff11-4790-4488-9564-7370724816c2");
+
+        mvc.perform(patch(ROLL_BACK_VERSION, id))
+                .andExpect(status().isOk());
+
+        ProductEntity productEntity = productRepository.findLastVersionById(id).orElse(null);
+        assertNotNull(productEntity);
+        assertEquals(id, productEntity.getId());
+        assertEquals(ProductEntity.State.ACTIVE, productEntity.getState());
+        assertEquals(0L, productEntity.getVersion());
     }
 
     @Test
-    void syncTariffTest() {
+    void syncTariffTest() throws Exception {
+        ProductNotification productNotification1 = new ProductNotification();
+        productNotification1.setProduct(UUID.fromString("a8ddef4d-5942-42b8-9354-41a715e03b56"));
+        productNotification1.setTariff(null);
+        productNotification1.setTariffVersion(null);
+        productNotification1.setStartDate(LocalDateTime.now());
+
+        ProductNotification productNotification2 = new ProductNotification();
+        productNotification2.setProduct(UUID.fromString("a929d899-1f06-418d-a002-77f4d6584676"));
+        productNotification2.setTariff(UUID.randomUUID());
+        productNotification2.setTariffVersion(1L);
+        productNotification2.setStartDate(LocalDateTime.now());
+
+        mvc.perform(post(SYNC_TARIFF).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(List.of(productNotification1, productNotification2))))
+                .andExpect(status().isOk());
+
+        ProductEntity productEntity1 = productRepository.findLastVersionById(productNotification1.getProduct()).orElse(null);
+        assertNotNull(productEntity1);
+        assertEquals(productNotification1.getProduct(), productEntity1.getId());
+        assertNull(productEntity1.getTariff());
+        assertNull(productEntity1.getTariffVersion());
+        assertEquals(ProductEntity.State.ACTIVE, productEntity1.getState());
+        assertEquals(1L, productEntity1.getVersion());
+
+        ProductEntity productEntity2 = productRepository.findLastVersionById(productNotification2.getProduct()).orElse(null);
+        assertNotNull(productEntity2);
+        assertEquals(productNotification2.getProduct(), productEntity2.getId());
+        assertEquals(productNotification2.getTariff(), productEntity2.getTariff());
+        assertEquals(productNotification2.getTariffVersion(), productEntity2.getTariffVersion());
+        assertEquals(ProductEntity.State.ACTIVE, productEntity2.getState());
+        assertEquals(1L, productEntity2.getVersion());
     }
 }
--- src/test/resources/db/migration/V10001__TestData.sql
+++ src/test/resources/db/migration/V10001__TestData.sql
@@ -6,3 +6,13 @@ INSERT INTO products (id, name, type, start_date, end_date, tariff, tariff_versi
 INSERT INTO products (id, name, type, start_date, end_date, tariff, tariff_version, author, version, state) VALUES
 ('15cfb4c1-7083-475e-838d-4a1e696cf917', 'Product Del Create', 'LOAN', '2020-01-01T12:00:00.000', '2020-01-01T13:00:00.000', null, null, '53d15658-5493-4828-80d9-f1c1f8eae252', 0, 'INACTIVE'),
 ('15cfb4c1-7083-475e-838d-4a1e696cf917', 'Product Del Update 1', 'LOAN', '2020-01-01T13:00:00.000', null, '38abfc79-bf9d-400a-808e-79256c28c401', 1, '53d15658-5493-4828-80d9-f1c1f8eae252', 1, 'ACTIVE');
+
+INSERT INTO products (id, name, type, start_date, end_date, tariff, tariff_version, author, version, state) VALUES
+('272fff11-4790-4488-9564-7370724816c2', 'Product RollBack Create', 'LOAN', '2020-01-01T12:00:00.000', '2020-01-01T13:00:00.000', null, null, '53d15658-5493-4828-80d9-f1c1f8eae252', 0, 'INACTIVE'),
+('272fff11-4790-4488-9564-7370724816c2', 'Product RollBack Update 1', 'LOAN', '2020-01-01T13:00:00.000', null, null, null, '53d15658-5493-4828-80d9-f1c1f8eae252', 1, 'DELETED');
+
+INSERT INTO products (id, name, type, start_date, end_date, tariff, tariff_version, author, version, state) VALUES
+('a8ddef4d-5942-42b8-9354-41a715e03b56', 'Product Del Create', 'LOAN', '2020-01-01T12:00:00.000', null, '7567c537-feca-4483-848f-cddddc4deb5a', 0, '53d15658-5493-4828-80d9-f1c1f8eae252', 0, 'ACTIVE');
+
+INSERT INTO products (id, name, type, start_date, end_date, tariff, tariff_version, author, version, state) VALUES
+('a929d899-1f06-418d-a002-77f4d6584676', 'Product RollBack Create', 'LOAN', '2020-01-01T12:00:00.000', null, null, null, '53d15658-5493-4828-80d9-f1c1f8eae252', 0, 'ACTIVE');
