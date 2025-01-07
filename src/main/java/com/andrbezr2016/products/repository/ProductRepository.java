9cf02c6b6e9ef52a6596350aad66f34569cab249
Bezrukov Andrey <andrbezr2016@yandex.ru>
2025-01-07 20:04:08 +0300

    add locks

--- src/main/java/com/andrbezr2016/products/repository/ProductRepository.java
+++ src/main/java/com/andrbezr2016/products/repository/ProductRepository.java
@@ -2,9 +2,12 @@ package com.andrbezr2016.products.repository;
 
 import com.andrbezr2016.products.entity.ProductEntity;
 import com.andrbezr2016.products.entity.ProductId;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,9 +17,13 @@ import java.util.UUID;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, ProductId> {
 
+    @Transactional
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("FROM ProductEntity WHERE id = :id AND state = 'ACTIVE'")
     Optional<ProductEntity> findCurrentVersionById(UUID id);
 
+    @Transactional
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("FROM ProductEntity WHERE (id, version) IN (SELECT id, MAX(version) FROM ProductEntity WHERE id = :id GROUP BY id)")
     Optional<ProductEntity> findLastVersionById(UUID id);
 
